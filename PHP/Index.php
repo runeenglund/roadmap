58db2a34a7d6771c8558c4707f222dc7fb69a026
AndKrogh <anders.krogh99@gmail.com>
2023-05-15 13:20:09 +0200

    making listview

--- PHP/Index.php
+++ PHP/Index.php
@@ -32,6 +32,42 @@
                     <h1>hej</h1>
                     <h1>hej</h1>
                 </div>
+                 <div class ="hej4">
+                    <h1>hej</h1>
+                    <h1>hej</h1>
+                    <h1>hej</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+     <div id="headerBox">
+        <div class="headerTitles">
+            <h1>hej</h1>
+            <h1>hej</h1>
+            <h1>hej</h1>
+        </div>
+        <div class ="outerBox">
+            <div class="innerBox">
+                <div class ="hej1">
+                    <h1>hej</h1>
+                    <h1>hej</h1>
+                    <h1>hej</h1>
+                </div>
+                <div class ="hej2">
+                    <h1>hej</h1>
+                    <h1>hej</h1>
+                    <h1>hej</h1>
+                </div>
+                <div class ="hej3">
+                    <h1>hej</h1>
+                    <h1>hej</h1>
+                    <h1>hej</h1>
+                </div>
+                 <div class ="hej4">
+                    <h1>hej</h1>
+                    <h1>hej</h1>
+                    <h1>hej</h1>
+                </div>
             </div>
         </div>
     </div>
