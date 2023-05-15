8829132449930a18914435a45510247f7bf8de04
AndKrogh <anders.krogh99@gmail.com>
2023-05-15 10:40:18 +0200

    linking css

--- PHP/Index.php
+++ PHP/Index.php
@@ -6,12 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="../Styles.css" />
 </head>
 <body>
     <div id="headerBox">
         <div class="innerBox">
             <div class ="outerBox">
-                <div>
+                <div class ="hej1">
                     <h1>hej</h1>
                 </div>
                 <div>
