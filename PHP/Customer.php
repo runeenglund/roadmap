1c74b7deb4f0e504748e84da73420b53424bfa67
runeenglund <rune_ye@hotmail.com>
2023-05-15 10:58:38 +0200

    boxes

--- PHP/Customer.php
+++ PHP/Customer.php
@@ -5,9 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kunder</title>
+    <link rel="stylesheet" type="text/css" href="../Styles.css" />
 </head>
 <body>
-    <h1>hej med dig</h1>
-    
+    <div class="box-container">
+        <div class="box">
+            <h2>overskrift1</h2>
+            <p>text1</p>
+        </div>
+        <div class="box">
+            <h2>overskrift2</h2>
+            <p>text2</p>
+        </div>
+        <div class="box">
+            <h2>overskrift3</h2>
+            <p>text3</p>
+        </div>
+    </div>
 </body>
 </html>
