f32c1625a5b720b552e7cc14dbe70f80e78f1a57
AndKrogh <anders.krogh99@gmail.com>
2023-05-16 14:11:29 +0200

    making table changes

--- PHP/Index.php
+++ PHP/Index.php
@@ -25,7 +25,7 @@
             <td>Igangværende</td>
             <td>1 kommentar</td>
           </tr>
-          <tr class="active-row">
+          <tr>
             <td>Dashboard</td>
             <td>2</td>
             <td>ventende</td>
@@ -55,7 +55,61 @@
             <td>Igangværende</td>
             <td>1 kommentar</td>
           </tr>
-          <tr class="active-row">
+          <tr>
+            <td>Dashboard</td>
+            <td>2</td>
+            <td>ventende</td>
+            <td>1 kommentar</td>
+          </tr>
+          <tr>
+            <td>Ændring i menubar</td>
+            <td>2</td>
+            <td>Igangvære</td>
+            <td>1 kommentar</td>
+          </tr>
+          <tr>
+            <td>Formular</td>
+            <td>2</td>
+            <td>Igangværende</td>
+            <td>1 kommentar</td>
+          </tr>
+          <tr>
+            <td>Dashboard</td>
+            <td>2</td>
+            <td>ventende</td>
+            <td>1 kommentar</td>
+          </tr>
+          <tr>
+            <td>Ændring i menubar</td>
+            <td>2</td>
+            <td>Igangvære</td>
+            <td>1 kommentar</td>
+          </tr>
+          <tr>
+            <td>Formular</td>
+            <td>2</td>
+            <td>Igangværende</td>
+            <td>1 kommentar</td>
+          </tr>
+          <tr>
+            <td>Dashboard</td>
+            <td>2</td>
+            <td>ventende</td>
+            <td>1 kommentar</td>
+          </tr>
+          <tr>
+            <td>Ændring i menubar</td>
+            <td>2</td>
+            <td>Igangvære</td>
+            <td>1 kommentar</td>
+          </tr>
+          <tr>
+            <td>Formular</td>
+            <td>2</td>
+            <td>Igangværende</td>
+            <td>1 kommentar</td>
+          </tr>
+          <tr>
             <td>Dashboard</td>
             <td>2</td>
             <td>ventende</td>
@@ -69,71 +123,5 @@
           </tr>
         </tbody>
       </table>
-<!--     <div id="top3HeaderBox">
-        <div class="top3HeaderTitles">
-            <h1>Navn</h1>
-            <h1>Bedømmelse</h1>
-            <h1>Status</h1>
-            <h1>Kommentar</h1>
-        </div>
-        <div class ="top3OuterBox">
-            <div class="top3InnerBox">
-                <div class ="hej1">
-                    <h1>Formular</h1>
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                </div>
-                <div class ="hej2">
-                    <h1>Nyt dashboard</h1>
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                </div>
-                <div class ="hej3">
-                    <h1>Opdatering af menu</h1>
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                </div>
-            </div>
-        </div>
-    </div>
-     <div id="headerBox">
-        <div class="headerTitles">
-            <h1>Navn</h1>
-            <h1>Bedømmelse</h1>
-            <h1>Status</h1>
-            <h1>Kommentar</h1>
-        </div>
-        <div class ="outerBox">
-            <div class="innerBox">
-                <div class ="hej1">
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                </div>
-                <div class ="hej2">
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                </div>
-                <div class ="hej3">
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                </div>
-                 <div class ="hej4">
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                    <h1>hej</h1>
-                </div>
-            </div>
-        </div>
-    </div> -->
 </body>
 </html>
