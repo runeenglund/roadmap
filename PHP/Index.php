00fbcb4e74ad132a7665f5c23869fc997b25adbb
AndKrogh <anders.krogh99@gmail.com>
2023-05-16 12:12:05 +0200

    making table changes

--- PHP/Index.php
+++ PHP/Index.php
@@ -9,6 +9,36 @@
     <link rel="stylesheet" type="text/css" href="../Styles.css" />
 </head>
 <body>
+      <table class="content-table">
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Points</th>
+            <th>Team</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Domenic</td>
+            <td>88,110</td>
+            <td>dcode</td>
+          </tr>
+          <tr class="active-row">
+            <td>2</td>
+            <td>Sally</td>
+            <td>72,400</td>
+            <td>Students</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Nick</td>
+            <td>52,300</td>
+            <td>dcode</td>
+          </tr>
+        </tbody>
+      </table>
     <div id="top3HeaderBox">
         <div class="top3HeaderTitles">
             <h1>Navn</h1>
