734a4cdc3780b1d4e68e44a224725c7feeafc57b
AndKrogh <anders.krogh99@gmail.com>
2023-05-18 09:34:09 +0200

    api/db con

--- PHP/dbCon/Api.php
+++ PHP/dbCon/Api.php
@@ -58,7 +58,7 @@
             $query=$conn->query($sql);
      
             if($query){
-                $out['message']='Bod Successfully Added';
+                $out['message']='Rask Successfully Added';
             }
             else{
                 $out['error']=true;
@@ -68,17 +68,17 @@
         }
     }
     
-    if($action=='updateboder'){
-        $bodnr = $_PUT['bod_nr'];
-        $bodnavn = $_PUT['bod_navn'];
-        $email = $_PUT['email'];
-        $adgangskode = $_PUT['adgangskode'];
+    if($action=='updatetasks'){
+        $navn = $_PUT['navn'];
+        $bedømmelse = $_PUT['bedømmelse'];
+        $status = $_PUT['status'];
+        $kommentar = $_PUT['kommentar'];
      
-        $sql="UPDATE boder SET bod_nr='$bodnr', bod_navn='$bodnavn', email='$email', adgangskode='$adgangskode'";
+        $sql="UPDATE tasks SET navn='$navn', bedømmelse='$bedømmelse', status='$status', kommentar='$kommentar'";
         $query=$conn->query($sql);
         
         if($query){
-            $out['message']='Bod Successfully Updatet';
+            $out['message']='Task Successfully Updatet';
         }
         else{
             $out['error']=true;
@@ -88,113 +88,11 @@
     
     if($action=='deleteboder'){
      
-        $sql="DELETE FROM boder WHERE bod_nr=$bodnr";
+        $sql="DELETE FROM tasks WHERE navn=$navn";
         $query=$conn->query($sql);
 
     }
     
-    
-    //Retter CRUD *************************
-    
-    if($action=='showretter'){
-    $sql = "SELECT *, allergener.allergen_id, 
-            CASE 
-            WHEN vegetar = TRUE THEN 'Vegetar'
-            WHEN vegansk = TRUE THEN 'Vegansk'
-            WHEN glutenfri = TRUE THEN 'Glutenfri' 
-            WHEN laktosefri = TRUE THEN 'Laktosefri'
-            ELSE ''
-            END mode
-            FROM retter
-            JOIN allergener
-            ON retter.allergen_id = allergener.allergen_id
-            ORDER BY bod_nr";
-    $query = $conn->query($sql);
-    $retter = array();
- 
-    while($row = $query->fetch_array()){
-        array_push($retter, $row);
-    }
- 
-    $out['retter'] = $retter;
-    }
-    
-    if($action=='addretter'){
-        $bodnr = $_POST['bod_nr'];
-        $navn = $_POST['navn'];
-        $billede = $_POST['billede'];
-        $beskrivelse = $_POST['beskrivelse'];
-        $pris = $_POST['pris'];
-        $allergen_id = $_POST['allergen_id'];
-        $kommentar = $_POST['kommentar'];
-     
-        if($bodnr==''){
-            $out['error']=true;
-            $out['message']='Add bod Failed. bodnr Empty.';
-        }
-        elseif($navn==''){
-            $out['error']=true;
-            $out['message']='Add bod Failed. navn Empty.';
-        }
-        elseif($billede==''){
-            $out['error']=true;
-            $out['message']='Add bod Failed. email Empty.';
-        }
-        elseif($beskrivelse==''){
-            $out['error']=true;
-            $out['message']='Add bod Failed. adgangskode Empty.';
-        }
-        elseif($pris==''){
-            $out['error']=true;
-            $out['message']='Add bod Failed. adgangskode Empty.';
-        }
-        elseif($allergen_id==''){
-            $out['error']=true;
-            $out['message']='Add bod Failed. adgangskode Empty.';
-        }
-        elseif($kommentar==''){
-            $out['error']=true;
-            $out['message']='Add bod Failed. adgangskode Empty.';
-        }
-        else{
-            $sql="INSERT INTO retter ( bod_nr, navn, billede, beskrivelse, pris, allergen_id, kommentar) VALUES ('$bodnr', '$navn', '$billede', '$beskrivelse', '$pris', '$allergen_id', '$kommentar')";
-            $query=$conn->query($sql);
-     
-            if($query){
-                $out['message']='Ret Successfully Added';
-            }
-            else{
-                $out['error']=true;
-                $out['message']='Error in Adding Occured';
-            }
-    
-        }
-    }
-    
-    if($action=='updateretter'){
-        $bodnr = $_PUT['bod_nr'];
-        $bodnavn = $_PUT['bod_navn'];
-        $email = $_PUT['email'];
-        $adgangskode = $_PUT['adgangskode'];
-     
-        $sql="UPDATE retter SET bod_nr='$bodnr', navn='$navn', billede='$billede', beskrivelse='$beskrivelse', pris='$pris', allergen_id='$allergen_id', kommentar='$kommentar'";
-        $query=$conn->query($sql);
-        
-        if($query){
-            $out['message']='Ret Successfully Updatet';
-        }
-        else{
-            $out['error']=true;
-            $out['message']='Error in Updating Occured';
-        }
-    }
-    
-    if($action=='deleteretter'){
-     
-        $sql="DELETE FROM retter WHERE navn=$navn";
-        $query=$conn->query($sql);
-
-    }
 
     //Close connection
     $conn->close();
